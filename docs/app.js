b3992853eecacc53bc2426a6cfba29b4141f21f3
Nanda Arya Putra <90232127+nandaarya@users.noreply.github.com>
2024-12-05 00:59:15 +0700

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -48,7 +48,7 @@ document.getElementById('chatForm').addEventListener('submit', async (e) => {
             const responseContainer = responseContainers[i];
 
             // Kirim instruksi ke backend untuk model saat ini
-            const response = await fetch(`https://165e-112-215-242-226.ngrok-free.app/api/chat/${modelKey}`, {
+            const response = await fetch(`https://d1e2-112-215-242-226.ngrok-free.app/api/chat/${modelKey}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
